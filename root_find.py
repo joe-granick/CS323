0fb82a9b7c78aa867ce9c13695f650de07107695
joe-granick <jgranick@pop-os.mynetworksettings.com>
2024-09-30 13:37:13 -0400

    newton method initial implementation, tests added not run

--- root_find.py
+++ root_find.py
@@ -1,6 +1,11 @@
 class FindRoot:
-    def newton_method():
-        pass
+    def newton_method(f,f_prime,x0,tol=10e-6):
+        po=x0
+        while True:
+            p1 =p0-f(p0)/f(po)
+            if abs(f(p1)-x0)<=tol:
+                return p1
+
 
     def secant_method():
         pass
@@ -8,4 +13,10 @@ class FindRoot:
 
     def bisection_method():
         pass
- 
+
+
+f = lambda x:x**2
+f_prime = lambda x:2*x
+x = 2
+print(newton_method(f,f_prime,x))
+
