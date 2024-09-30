d02583d478205eeb9daa6be9836813ac39fdd600
joe-granick <jgranick@pop-os.mynetworksettings.com>
2024-09-30 19:06:10 -0400

    secant method initial implementation

--- root_find.py
+++ root_find.py
@@ -1,5 +1,6 @@
+import math
 class FindRoot:
-    def newton_method(f,f_prime,x0,tol=10e-6):
+    def newton_method(f,f_prime,x0,tol=10**-6):
         """
         approximates root of a function
         takes the function and divides by the derivative
@@ -14,23 +15,103 @@ class FindRoot:
         """
         p0=x0
         while True:
-            p1=p0-((f(p0)-x0)/f(p0))
+            p1=p0-((f(p0))/f_prime(p0))
             print(p1)
-            if abs(f(p1)-x0)<=tol:
+            if abs(p1-p0)<tol:
                 return p1
             p0=p1
 
 
-    def secant_method():
-        pass
+    def secant_method(f, x0,x1,tol=10**-6):
+        p0,p1 = x0,x1
+
+        while True:
+            t=p1
+            p1=(f(p1)-f(p0))/(p1-p0)
+            p0=t 
+            if abs(p1-p0)<tol:
+                return p1
+
+
         
 
     def bisection_method():
         pass
 
-
-f = lambda x:x**2
+#test newton_method
+f = lambda x:x**2-2
 f_prime = lambda x:2*x
 x = 2
 print(FindRoot.newton_method(f,f_prime,x))
+print()
+
+#problem 1
+print("Problem 1")
+f = lambda x:-(2*x)/math.e**(x/2)+1
+f_prime = lambda x:(x/(math.e**(x/2)) - 2/(math.e**(x/2)))
+x = 0
+print("Newton's Method: ", FindRoot.newton_method(f,f_prime,x))
+print()
+
+#problem 2 
+print("Problem 2")
+f = lambda x:5-x**-1
+f_prime = lambda x:1/(x**2)
+x = (1/4)
+print("Newton's Method: ",FindRoot.newton_method(f,f_prime,x))
+print()
+#problem 2
+#f = lambda x:1-2*x*math.e**(-x/2)
+#f_prime = lambda x:(x-2)*math.e**(-x/2)
+#x = 0
+#print(FindRoot.newton_method(f,f_prime,0))
+
+#problem 3 
+#f = lambda x:1-2*x*math.e**(-x/2)
+#f_prime = lambda x:(x-2)*math.e**(-x/2)
+#x = 0
+#print(FindRoot.newton_method(f,f_prime,0))
+
+#problem 4
+#f = lambda x:1-2*x*math.e**(-x/2)
+#f_prime = lambda x:(x-2)*math.e**(-x/2)
+#x = 0
+#print(FindRoot.newton_method(f,f_prime,0))
+
+#problem 5
+#f = lambda x:1-2*x*math.e**(-x/2)
+#f_prime = lambda x:(x-2)*math.e**(-x/2)
+#x = 0
+#print(FindRoot.newton_method(f,f_prime,0))
+
+#problem 6
+#f = lambda x:1-2*x*math.e**(-x/2)
+#f_prime = lambda x:(x-2)*math.e**(-x/2)
+#x = 0
+#print(FindRoot.newton_method(f,f_prime,0))
+
+#problem 7
+#f = lambda x:1-2*x*math.e**(-x/2)
+#f_prime = lambda x:(x-2)*math.e**(-x/2)
+#x = 0
+#print(FindRoot.newton_method(f,f_prime,0))
+
+#problem 8
+#f = lambda x:1-2*x*math.e**(-x/2)
+#f_prime = lambda x:(x-2)*math.e**(-x/2)
+#x = 0
+#print(FindRoot.newton_method(f,f_prime,0))
+
+#problem 9
+#f = lambda x:1-2*x*math.e**(-x/2)
+#f_prime = lambda x:(x-2)*math.e**(-x/2)
+#x = 0
+#print(FindRoot.newton_method(f,f_prime,0))
+
+#problem 10
+#f = lambda x:1-2*x*math.e**(-x/2)
+#f_prime = lambda x:(x-2)*math.e**(-x/2)
+#x = 0
+#print(FindRoot.newton_method(f,f_prime,0))
+
 
