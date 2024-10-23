3a2c0282b61ca9576ce1f6ff4664bc2caf470813
joe-granick <jgranick@pop-os.mynetworksettings.com>
2024-10-22 21:23:10 -0400

    newton interpolation divided difference implemented

--- Assignment3/newton_interpolation.py
+++ Assignment3/newton_interpolation.py
@@ -1,5 +1,17 @@
-def newton_polynomial():
+def newton_polynomial
     pass
-def div_diff():
+def div_diff(x=[], y=[],f=lambda x: e**x, order = 1):
+        """ 
+        
+        calculates coefficient for next order of polynomial
+        
+        """
+    
+    coef=[] #table of coefficients for degree polynomial
+    for i in range(len(x)-1):
+       coef[i] = (y[i+1] - y[i])/(x[i+1]-x[i])
+        return coef
+
+
+def generate_uniform_samples(n=501):
     pass
-            
